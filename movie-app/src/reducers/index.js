d30c1ec0b605f087833ab7b0572b3f1c76dff6c5
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-04-26 12:02:08 +0530

    add the  reducers and pure functions

---
+++ movie-app/src/reducers/index.js
@@ -0,0 +1,8 @@
+/// Make reducers function as the pure functions
+
+export default function movies(state = [], action) {
+if(action.type === 'ADD_MOVIES'){
+  return action.movies;
+}
+return state; 
+}
