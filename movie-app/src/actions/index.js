4b3514f605d6ea4e4a93c083a35e7096848e31de
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-05-01 00:32:01 +0530

    add functionlity of search and use redux-thunk

--- movie-app/src/actions/index.js
+++ movie-app/src/actions/index.js
@@ -9,6 +9,7 @@ export const ADD_MOVIES = 'ADD_MOVIES';
 export const ADD_TO_FAVOURITE = 'ADD_TO_FAVOURITE';
 export const REMOVE_FROM_FAVOURITE = 'REMOVE_FROM_FAVOURITE';
 export const SET_SHOW_FAVOURITE = 'SET_SHOW_FAVOURITE';
+export const ADD_MOVIE_TO_LIST = 'ADD_MOVIE_TO_LIST';
 
 
 
@@ -39,4 +40,26 @@ export function setShowFavourite (val) {
       type: SET_SHOW_FAVOURITE,
       val
     }
+}
+
+export function addMovieToList (movie) {
+  return {
+      type: ADD_MOVIE_TO_LIST,
+      movie: movie
+    }
+}
+
+export function handleMovieSearch (movie) {
+  const url = `http://www.omdbapi.com/?apiKey=3ca5df7&t=${movie}`;
+
+  return function (dispatch){
+    fetch(url)
+    .then(response => response.json())
+    .then(movie => {
+    console.log('movie', movie)
+
+    /// dispatch an action 
+    // dispatch({type: 'ADD_SEARCH_RESULT', movie})
+     })
+  }
 }
--- movie-app/src/index.js
+++ movie-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {createStore, applyMiddleware} from 'redux';
+// import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './components/App';
@@ -21,12 +22,22 @@ import rootReducer from './reducers';
 /// modify middleware 
 const logger = ({dispatch, getState}) => (next) => (action) => {
   /// logger code
+   if(typeof action !== 'function'){
     console.log('ACTION_TYPE =', action.type);
+   }
     next(action);
 }
 
+const thunk = ({dispatch, getState}) => (next) => (action) => {
+  /// logger code
+  if(typeof action === 'function'){
+    action(dispatch);
+    return;
+  }
+    next(action);
+}
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 // store.dispatch({
 //   type: 'ADD_MOVIES',
